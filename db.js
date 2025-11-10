88edcdd52ac4d591b40350a2d34699ecb9f338fe
Baptista Valeta <gabryelvaleta@gmail.com>
2025-11-10 01:43:31 +0100

    Novos arquivos models e controllers adicionados ao projecto

--- db.js
+++ db.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose'); // importar mongoose
 
 const dbUri = "mongodb+srv://admin:<admin1234>@cluster0.4khd2dh.mongodb.net/employee_db?appName=Cluster0"
 
+mongoose.set('strictQuery', false);
+
 // exportando a função
 module.exports = () => {
-    return mongoose.connect(dbUri, 
-        {userNewUrlParser: true, useUnifiedTopology: true}
-    );
+    return mongoose.connect(dbUri);
 }  
--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const bodyParser = require('body-parser'); // Para converter qualquer pedido fei
 
 // local imports
 const connectDb = require('./db.js');
+const employeeRoutes = require("./controllers/employee.controller.js")
 
 const app = express(); // receba o valor retornado pelo método express
 
 // middleware
 app.use(bodyParser.json());
+app.use("/api/emplyees", employeeRoutes)
 
 
 connectDb()
@@ -19,4 +21,7 @@ connectDb()
         // Chamar o método listen para iniciar o servidor
         app.listen(3000, () => console.log('Servidor iniciado na porta 3000'));
     })
-    .catch(err => console.error(err))
+    .catch(err => console.error('Erro ao iniciar a aplicação: ',err))
+
+
+
